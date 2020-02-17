80d4b946c82c2ac164d62f55093161f7240be366
j-bd <recodata.out@gmail.com>
2020-02-17 18:46:57 +0100

    Initial commit

---
+++ load_display_save.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 18:44:59 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+import cv2
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
