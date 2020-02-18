89a3122974871277db8740eb3ebd615f0d6e50af
j-bd <recodata.out@gmail.com>
2020-02-18 15:51:07 +0100

    initial commit

---
+++ 4-Basic/getting_and_setting.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 15:49:16 2020
+
+@author: j-bd
+"""
+
+
+
+
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
