f628cc5a63d464015cf0d1cd36c81cac4c4c4fb9
j-bd <recodata.out@gmail.com>
2020-02-18 15:57:12 +0100

    parse and display

--- 4-Basic/getting_and_setting.py
+++ 4-Basic/getting_and_setting.py
@@ -5,14 +5,43 @@ Created on Tue Feb 18 15:49:16 2020
 
 @author: j-bd
 """
+import argparse
+
+import cv2
 
 
+def arguments_parser():
+    '''Retrieve user commands'''
+    parser = argparse.ArgumentParser(
+        prog="Pixel",
+        usage='''%(prog)s [OpenCV]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch execution:
+        -------------------------------------
+        python3 getting_and_setting.py
+        --image "path/to/image/directory"
 
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-i", "--image", required=True, help="Path to the image"
+    )
+    args = vars(parser.parse_args())
+    return args
 
-
+def display(path):
+    '''Display image'''
+    image = cv2.imread(path)
+    cv2.imshow("Image", image)
+    cv2.waitKey()
+    return image
 
 def main():
     '''Launch main steps'''
+    args = arguments_parser()
+
+    image = display(args["image"])
 
 
 if __name__ == "__main__":
