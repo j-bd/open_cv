d0c7164489ed02a720e7f00e68fa2c7c4a57b397
j-bd <recodata.out@gmail.com>
2020-02-18 14:50:00 +0100

    argument parser

--- 3-Initial/load_display_save.py
+++ 3-Initial/load_display_save.py
@@ -11,8 +11,29 @@ import argparse
 import cv2
 
 
+def arguments_parser():
+    '''Retrieve user commands'''
+    parser = argparse.ArgumentParser(
+        prog="Basic function",
+        usage='''%(prog)s [OpenCV]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch execution:
+        -------------------------------------
+        python3 load_display_save.py
+        --image "path/to/image/directory"
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-i", "--image", required = True, help = "Path to the image"
+    )
+    args = vars(parser.parse_args())
+    return args
+
 def main():
     '''Launch main steps'''
+    args = arguments_parser()
 
 
 if __name__ == "__main__":
