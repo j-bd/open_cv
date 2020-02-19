c4d970e5e2d0efc6b4b5ab56570cafd504ad3011
j-bd <recodata.out@gmail.com>
2020-02-19 16:30:01 +0100

    initial commit

---
+++ 5-Drawing/drawing.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 16:28:26 2020
+
+@author: j-bd
+"""
+
+import numpy as np
+import cv2
+
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
