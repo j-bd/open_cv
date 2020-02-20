e8a14df2991a06d391ccfdc7865495609a3c4b17
j-bd <recodata.out@gmail.com>
2020-02-20 10:46:59 +0100

    initial commit

---
+++ 6-Image_processing/translation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 10:44:49 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+import numpy as np
+import imutils
+import cv2
+
+
+
+
+def main():
+    '''Launch main steps'''
+
+
+
+
+if __name__ == "__main__":
+    main()
