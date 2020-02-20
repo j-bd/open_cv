c61888480889be37b08109177f99a177119d498f
j-bd <recodata.out@gmail.com>
2020-02-20 15:59:06 +0100

    initial commit

---
+++ 6-Image_processing/rotate.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 15:54:22 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+
+def arguments_parser():
+    '''Retrieve user commands'''
+    parser = argparse.ArgumentParser(
+        prog="Rotation",
+        usage='''%(prog)s [OpenCV]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch execution:
+        -------------------------------------
+        python3 rotation.py
+        --image "path/to/image/directory"
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-i", "--image", required=True, help="Path to the image"
+    )
+    args = vars(parser.parse_args())
+    return args
+
+
+def main():
+    '''Launch main steps'''
+    args = arguments_parser()
+
+
+if __name__ == "__main__":
+    main()
