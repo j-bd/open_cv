139dfa5d3448a38e1653dfbc9f3406314fcbff50
j-bd <recodata.out@gmail.com>
2020-02-20 16:28:28 +0100

    rotation implement

--- 6-Image_processing/rotate.py
+++ 6-Image_processing/rotate.py
@@ -8,6 +8,9 @@ Created on Thu Feb 20 15:54:22 2020
 
 import argparse
 
+import cv2
+import numpy as np
+
 
 def arguments_parser():
     '''Retrieve user commands'''
@@ -29,10 +32,26 @@ def arguments_parser():
     args = vars(parser.parse_args())
     return args
 
+def rotate(image):
+    '''Proceed rotation'''
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    mat = cv2.getRotationMatrix2D(center, 45, 1.0)
+    rotated = cv2.warpAffine(image, mat, (w, h))
+    cv2.imshow("Rotated by 45 Degrees", rotated)
+
+    mat = cv2.getRotationMatrix2D(center, -90, 2.0)
+    rotated = cv2.warpAffine(image, mat, (w, h))
+    cv2.imshow("Rotated by -90 Degrees", rotated)
 
 def main():
     '''Launch main steps'''
     args = arguments_parser()
+
+    image = cv2.imread(args["image"])
+    cv2.imshow("Original", image)
+
+    image = rotate(image)
 
 
 if __name__ == "__main__":
