cad3e1b93f758ef587362e5c713bc18f8c96b941
j-bd <recodata.out@gmail.com>
2020-02-21 15:03:05 +0100

    resizing implementation

--- 6-Image_processing/resize.py
+++ 6-Image_processing/resize.py
@@ -6,11 +6,9 @@ Created on Thu Feb 20 16:30:40 2020
 @author: j-bd
 """
 
-
 import argparse
 
 import cv2
-import numpy as np
 
 
 def arguments_parser():
@@ -32,6 +30,15 @@ def arguments_parser():
     )
     args = vars(parser.parse_args())
     return args
+
+def resize(image):
+    '''Resize input image'''
+    # Ratio computation
+    ratio = 150.0 / image.shape[1] # width
+
+    new_dim = (150, int(image.shape[0] * ratio)) # (width, height)
+    resized = cv2.resize(image, new_dim, interpolation=cv2.INTER_AREA)
+    cv2.imshow("Resized (Width)", resized)
 
 def main():
     '''Launch main steps'''
