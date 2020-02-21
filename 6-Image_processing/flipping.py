f3ccfdf6acd9ba787145798f54e992daa4858179
j-bd <recodata.out@gmail.com>
2020-02-21 15:32:23 +0100

    flipping implementation

--- 6-Image_processing/flipping.py
+++ 6-Image_processing/flipping.py
@@ -31,12 +31,26 @@ def arguments_parser():
     args = vars(parser.parse_args())
     return args
 
+def flip(image):
+    '''Realize different flipping'''
+    flipped = cv2.flip(image, 1)
+    cv2.imshow("Flipped Horizontally", flipped)
+
+    flipped = cv2.flip(image, 0)
+    cv2.imshow("Flipped Vertically", flipped)
+
+    flipped = cv2.flip(image, -1)
+    cv2.imshow("Flipped Horizontally & Vertically", flipped)
+    cv2.waitKey()
+
 def main():
     '''Launch main steps'''
     args = arguments_parser()
 
     image = cv2.imread(args["image"])
     cv2.imshow("Original", image)
+
+    flip(image)
 
 
 if __name__ == "__main__":
