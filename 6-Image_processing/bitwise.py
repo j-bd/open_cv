465c82b6804aa52c92187de0603f8c120ca6c6dd
j-bd <recodata.out@gmail.com>
2020-02-21 18:10:46 +0100

    bitwise operation

---
+++ 6-Image_processing/bitwise.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 21 17:42:42 2020
+
+@author: j-bd
+"""
+
+import numpy as np
+import cv2
+
+
+rectangle = np.zeros((300, 300), dtype="uint8")
+cv2.rectangle(rectangle, (25, 25), (275, 275), 255, -1)
+cv2.imshow("Rectangle", rectangle)
+
+circle = np.zeros((300, 300), dtype="uint8")
+cv2.circle(circle, (150, 150), 150, 255, -1)
+cv2.imshow("Circle", circle)
+
+# Bitwise operation
+bitwise_and = cv2.bitwise_and(rectangle, circle)
+cv2.imshow("AND", bitwise_and)
+cv2.waitKey()
+
+bitwise_or = cv2.bitwise_or(rectangle, circle)
+cv2.imshow("OR", bitwise_or)
+cv2.waitKey()
+
+bitwise_xor = cv2.bitwise_xor(rectangle, circle)
+cv2.imshow("XOR", bitwise_xor)
+cv2.waitKey()
+
+bitwise_not = cv2.bitwise_not(circle)
+cv2.imshow("NOT", bitwise_not)
+cv2.waitKey()
