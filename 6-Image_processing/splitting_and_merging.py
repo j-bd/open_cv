fd176355572bda549c64f52db070543f41fd4bcc
j-bd <recodata.out@gmail.com>
2020-02-22 12:13:33 +0100

    split and merge implementation

--- 6-Image_processing/splitting_and_merging.py
+++ 6-Image_processing/splitting_and_merging.py
@@ -32,12 +32,29 @@ def arguments_parser():
     args = vars(parser.parse_args())
     return args
 
+def split_merge(image):
+    '''Realize image split and merge'''
+    (ch_b, ch_g, ch_r) = cv2.split(image)
+    cv2.imshow("Red", ch_r)
+    cv2.imshow("Green", ch_g)
+    cv2.imshow("Blue", ch_b)
+
+    merged = cv2.merge([ch_b, ch_g, ch_r])
+    cv2.imshow("Merged", merged)
+
+    zeros = np.zeros(image.shape[:2], dtype="uint8")
+    cv2.imshow("Red", cv2.merge([zeros, zeros, ch_r]))
+    cv2.imshow("Green", cv2.merge([zeros, ch_g, zeros]))
+    cv2.imshow("Blue", cv2.merge([ch_b, zeros, zeros]))
+
 def main():
     '''Launch main steps'''
     args = arguments_parser()
 
     image = cv2.imread(args["image"])
     cv2.imshow("Original", image)
+
+    split_merge(image)
 
 
 if __name__ == "__main__":
