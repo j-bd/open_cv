d6c941067f6ccba1715e3afa69dafbef0d75462e
j-bd <recodata.out@gmail.com>
2020-02-22 11:51:42 +0100

    initial commit

---
+++ 6-Image_processing/splitting_and_merging.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 11:50:10 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+import cv2
+import numpy as np
+
+
+def arguments_parser():
+    '''Retrieve user commands'''
+    parser = argparse.ArgumentParser(
+        prog="Splitting and merging",
+        usage='''%(prog)s [OpenCV]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch execution:
+        -------------------------------------
+        python3 splitting_and_merging.py
+        --image "path/to/image/directory"
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-i", "--image", required=True, help="Path to the image"
+    )
+    args = vars(parser.parse_args())
+    return args
+
+def main():
+    '''Launch main steps'''
+    args = arguments_parser()
+
+    image = cv2.imread(args["image"])
+    cv2.imshow("Original", image)
+
+
+if __name__ == "__main__":
+    main()
