3bc5ab91d132e2e31f5156b36f6faa87f5ab6913
j-bd <recodata.out@gmail.com>
2020-02-22 10:59:02 +0100

    initial commit

---
+++ 6-Image_processing/masking.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 10:56:00 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+import cv2
+import numpy as np
+
+
+def arguments_parser():
+    '''Retrieve user commands'''
+    parser = argparse.ArgumentParser(
+        prog="Masking",
+        usage='''%(prog)s [OpenCV]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch execution:
+        -------------------------------------
+        python3 masking.py
+        --image "path/to/image/directory"
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-i", "--image", required=True, help="Path to the image"
+    )
+    args = vars(parser.parse_args())
+    return args
+
+def main():
+    '''Launch main steps'''
+    args = arguments_parser()
+
+    image = cv2.imread(args["image"])
+    cv2.imshow("Original", image)
+
+
+if __name__ == "__main__":
+    main()
+
