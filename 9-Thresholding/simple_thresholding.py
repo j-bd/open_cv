63651078a3e2cf3b0128bc55e876f2d355bb8986
j-bd <recodata.out@gmail.com>
2020-02-24 17:00:40 +0100

    simple thresholding implementation

--- 9-Thresholding/simple_thresholding.py
+++ 9-Thresholding/simple_thresholding.py
@@ -33,12 +33,27 @@ def arguments_parser():
     args = vars(parser.parse_args())
     return args
 
+def thresholding(image, blurred):
+    '''Apply predefined threshold'''
+    (th_v, th_im) = cv2.threshold(blurred, 80, 255, cv2.THRESH_BINARY)
+    cv2.imshow("Threshold Binary", th_im)
+    (th_v, th_im_inv) = cv2.threshold(blurred, 80, 255, cv2.THRESH_BINARY_INV)
+    cv2.imshow("Threshold Binary Inverse", th_im_inv)
+
+    cv2.imshow("Masked", cv2.bitwise_and(image, image, mask=th_im_inv))
+
+
 def main():
     '''Launch main steps'''
     args = arguments_parser()
 
-    image = cv2.imread(args["image"])
+    image = cv2.imread(args["image"], 0)
     cv2.imshow("Original", image)
+
+    blurred = cv2.GaussianBlur(image, (5, 5), 0)
+    cv2.imshow("blurred", blurred)
+
+    thresholding(image, blurred)
 
 
 if __name__ == "__main__":
