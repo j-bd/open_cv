a0d188f9ff72973fe3612098f3f6f7fd6268b0ef
j-bd <recodata.out@gmail.com>
2020-02-24 10:00:23 +0100

    initial commit

---
+++ 8-Smooth_blur/blurring.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 09:58:54 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def arguments_parser():
+    '''Retrieve user commands'''
+    parser = argparse.ArgumentParser(
+        prog="Histogram with mask",
+        usage='''%(prog)s [OpenCV]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch execution:
+        -------------------------------------
+        python3 histogram_with_mask.py
+        --image "path/to/image/directory"
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-i", "--image", required=True, help="Path to the image"
+    )
+    args = vars(parser.parse_args())
+    return args
+
+def main():
+    '''Launch main steps'''
+    args = arguments_parser()
+
+    image = cv2.imread(args["image"])
+    cv2.imshow("Original", image)
+
+
+if __name__ == "__main__":
+    main()
+
