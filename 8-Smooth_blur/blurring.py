6b7d32406674cdc47f8ab428a17a343d71e42997
j-bd <recodata.out@gmail.com>
2020-02-24 10:41:19 +0100

    gaussian blur

--- 8-Smooth_blur/blurring.py
+++ 8-Smooth_blur/blurring.py
@@ -37,9 +37,17 @@ def averaging_blur(image):
     '''Compute blur image with predefined kernel'''
     blurred = np.hstack(
         [image, cv2.blur(image, (3, 3)), cv2.blur(image, (5, 5)),
-         cv2.blur(image, (7, 7))]
+        cv2.blur(image, (7, 7))]
     )
     cv2.imshow("Averaged", blurred)
+
+def gaussian_blur(image):
+    '''Compute blur image with predefined kernel'''
+    blurred = np.hstack(
+        [image, cv2.GaussianBlur(image, (3, 3), 0),
+        cv2.GaussianBlur(image, (5, 5), 0), cv2.GaussianBlur(image, (7, 7), 0)]
+    )
+    cv2.imshow("Gaussian", blurred)
 
 def main():
     '''Launch main steps'''
@@ -49,6 +57,7 @@ def main():
     cv2.imshow("Original", image)
 
     averaging_blur(image)
+    gaussian_blur(image)
 
 
 if __name__ == "__main__":
