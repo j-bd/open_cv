ef6b5e9defb61dae32f8f48591c95583354a5fdd
j-bd <recodata.out@gmail.com>
2020-02-25 15:21:10 +0100

    initial commit

---
+++ 11-Contours/counting_coins.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 15:19:09 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+import cv2
+import numpy as np
+
+
+def arguments_parser():
+    '''Retrieve user commands'''
+    parser = argparse.ArgumentParser(
+        prog="Counting coins",
+        usage='''%(prog)s [OpenCV]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch execution:
+        -------------------------------------
+        python3 counting_coins.py
+        --image "path/to/image/directory"
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-i", "--image", required=True, help="Path to the image"
+    )
+    args = vars(parser.parse_args())
+    return args
+
+def main():
+    '''Launch main steps'''
+    args = arguments_parser()
+
+    image = cv2.imread(args["image"], 0)
+    cv2.imshow("Original", image)
+
+if __name__ == "__main__":
+    main()
